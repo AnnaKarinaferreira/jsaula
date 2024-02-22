08d66a8b29fca077425a26875618a20e5996903e
karinaferz <127227830+AnnaKarinaferreira@users.noreply.github.com>
2024-02-22 15:11:13 -0300

    Add files via upload

---
+++ variaveis/script.js
@@ -0,0 +1,34 @@
+var nome = "Anna",
+    idade = "17",
+    possuiFaculdade = "nao";
+
+console.log (nome, idade, possuiFaculdade)
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = totalComprado * preco;
+
+console.log (totalPreco)
+
+//variavel simplificada 
+var sobrenome = "Pacheco",
+    cidade = "SC";
+
+    console.log(sobrenome, cidade)
+
+
+//variavel nao definida
+var semDefinir;
+
+console.log(semDefinir)
+
+var nome = "karina",
+    idade = "17",
+    comida = "uva"
+    digiteum = "1"
+    digitedois = "dois"
+    digitetres = "tres"
+    digitequatro = "quatro"
+    anodeNascimento  = "2006"
+
+    console.log(nome, idade, comida, digiteum, digitedois, digitetres, digitequatro, anodeNascimento)
